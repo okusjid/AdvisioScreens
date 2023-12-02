b9b6967b323c743cc7454a3bdd1ef7b8f9772db8
USJID <usjidn@gmail.com>
2023-12-03 00:01:20 +0500

    Added Basic Signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import Login from "./pages/Login";
+//import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 export default function App() {
   return (
-  <Login />    
+  <Signup />    
   )
 }
