6dcd105a055e22f46afb3d0a30a2ac7ecfdfbd9d
USJID <usjidn@gmail.com>
2024-01-13 23:02:10 +0500

    Fixed Hero Sec

--- FRONTEND/src/components/Header.jsx
+++ FRONTEND/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <nav className="bg-white border-gray-200 px-4 lg:px-4 py-2 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">AdvisoScreens</span>
--- FRONTEND/src/components/Hero-Home.jsx
+++ FRONTEND/src/components/Hero-Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import billboardImage from "../assets/Billboard.png"; 
 
 const HeroSection = () => {
   return (
-    <section className="bg-black text-white text-center py-24 px-6 bg-cover bg-center" style={{ backgroundImage: "url('path-to-your-background-image.jpg')" }}>
-      <h1 className="text-6xl font-extrabold text-red-600 mb-4 leading-tight">Elevate Your Brand Visibility</h1>
-      <p className="text-2xl mb-6 max-w-xl mx-auto">Transform the way you engage with your audience using our state-of-the-art digital billboards. Affordable, effective, and tailor-made for your business needs.</p>
-      <Link to="/signup" className="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-8 rounded-full text-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
+    <section className="bg-black text-white text-center py-24 px-6 bg-cover bg-center" 
+             style={{ 
+               backgroundImage: `url(${billboardImage})`,
+             }}>
+      <h1 style={{textAlign: "left", fontSize: 52}} className="font-extrabold text-red-600 mb-4 leading-tight ">
+        Elevate Your Brand Visibility
+      </h1>
+      <p style={{textAlign: "left", fontSize: 20}} className="mb-6 max-w-xl ">
+        Transform the way you engage with your audience using our state-of-the-art digital billboards. Affordable, effective, and tailor-made for your business needs.
+      </p>
+      <Link to="/signup" 
+            className="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-8 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1">
         Start Your Journey
       </Link>
     </section>
