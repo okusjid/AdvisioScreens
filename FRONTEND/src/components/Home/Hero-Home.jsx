1d585d540869f5db294b4e182452b1b2471ede54
USJID <usjidn@gmail.com>
2024-01-27 17:59:05 +0500

    Updated home

--- FRONTEND/src/components/Home/Hero-Home.jsx
+++ FRONTEND/src/components/Home/Hero-Home.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
       <p style={{textAlign: "left", fontSize: 20}} className="mb-6 max-w-xl ">
         Transform the way you engage with your audience using our state-of-the-art digital billboards. Affordable, effective, and tailor-made for your business needs.
       </p>
-      <Link to="/signup" 
+      <Link to="/Howitworks" 
             className="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-8 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1">
         Start Your Journey
       </Link>
