34b1a660001e07641817a800d4ab3d9521bb2dd5
USJID <usjidn@gmail.com>
2024-01-30 21:06:05 +0500

    Updated Tagline Component

--- FRONTEND/src/components/Home/Tagline.jsx
+++ FRONTEND/src/components/Home/Tagline.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 // CTA Section Component
-const Tagline = () => {
+const Tagline = ({Tag1,Tag2,Tag3}) => {
     return (
       <section className="bg-indigo-300 py-8 text-center">
         <h2 className="text-3xl md:text-4xl font-semibold text-gray-800">
-          Innovation in Action - <span className="block text-indigo-700 md:inline-block">The Proof is in the Numbers</span>
+          {Tag1} - <span className="block text-indigo-700 md:inline-block">{Tag2}</span>
         </h2>
         <p className="mt-4 text-md md:text-lg text-gray-600 max-w-2xl mx-auto">
-          Discover the difference with data-driven results
+          {Tag3}
         </p>
       </section>
     );
--- FRONTEND/src/pages/How-it-works.jsx
+++ FRONTEND/src/pages/How-it-works.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Stepper from '../components/How It Works/Stepper'; 
-
+import Testimonials from '../components/Home/Testimonials'; 
+import Tag from '../components/Home/Tagline'; 
 const Howitworks = () =>
 {
     return(
         <>
         <Stepper />
+        <Tag Tag1 = "Your Partner" Tag2 = "in Growth and Success." Tag3 = "Where Success Stories Begin."/>
+        <Testimonials />
         </>
     )
 }
